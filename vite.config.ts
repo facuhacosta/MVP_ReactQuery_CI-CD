292ee193e26f78b84478ecb4d60920eeb841f3e3
Facundo <acosta.facundo.h@gmail.com>
2023-10-16 15:21:57 -0300

    feat: added threshholds to test coverage

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   test: {
-    environment: "happy-dom"
+    environment: "happy-dom",
+    coverage: {
+      provider: "v8",
+      lines: 80,
+      branches: 80,
+      functions: 80,
+      statements: 80
+    }
   }
 })
