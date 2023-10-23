beb07435a5dddabdca2cc6b227a677bbc422e72e
facuhacosta <facuacosta200997@hotmail.com>
2023-10-23 17:37:21 -0300

    feat: trying bun in CI

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,36 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import md5 from 'md5'
+import { useQuery } from '@tanstack/react-query'
+
+
+const fetchUsers = async () => {
+  return await fetch(`http://gateway.marvel.com/v1/public/comics?ts=${Date.now()}&apikey=${import.meta.env.VITE_PUBLIC_KEY}&hash=${md5(Date.now() + import.meta.env.VITE_PRIVATE_KEY + import.meta.env.VITE_PUBLIC_KEY)}`)
+    .then(async res => {
+      if (!res.ok) throw new Error("Error del fetch");
+      return await res.json()
+    })
+    .then(res => res.data.results)
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const {isLoading, isError, data} = useQuery(
+    ['comics'],
+    async () => {
+      await fetchUsers()
+    }
+  )
 
+  console.log(data);
+  
+  const [count, setCount] = useState(0)
+  const [comics, setComics] = useState(Array<any>)
+
+  useEffect(() => {
+    console.log(comics);
+  }, [comics])
   return (
     <>
       <div>
@@ -28,6 +53,10 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <div>
+        {isLoading && "Loading ..."}
+        {isError && "Error en la Carga"}
+      </div>
     </>
   )
 }
