e795fcd646f6f9fc4cfec55b4f75a8880cdfe2b4
facuhacosta <69871078+facuhacosta@users.noreply.github.com>
2023-11-02 21:27:56 -0300

    Page overall view (#11)
    * feat: changed pipelines names
    
    * feat: added main view and tests
    
    * feat: Fixed linter errors

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,24 @@
+import { Character } from "../../assets/types"
+import styles from "./Card.module.scss"
+
+const Card = ({character}:{character: Character}) => {  
+  return (
+    <div className={styles.container}>
+      <div className={styles.imageContainer}>
+        <img src={`${character.thumbnail.path}.${character.thumbnail.extension}`} alt="" />
+      </div>
+      <section>
+        <h3>{character.name}</h3>
+      </section>
+      <ol>
+        {
+          character.comics.items.slice(0,3).map(({name}) => (
+            <li>{name}</li>
+          ))
+        }
+      </ol>
+    </div>
+  )
+}
+
+export default Card
